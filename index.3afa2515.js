e4aea38435859cbe2d05afd9272958eb30acfd70
OlenaDidkivska <99613214+OlenaDidkivska@users.noreply.github.com>
2022-09-10 20:09:58 +0000

    Deploying to gh-pages from @ OlenaDidkivska/goit-js-hw-11@a2510da6bfddf8f0ade0154e45366098c37e0284 🚀

---
+++ index.3afa2515.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#search-form");console.log(e);e.addEventListener("submit",onSearchBtn);
+//# sourceMappingURL=index.3afa2515.js.map
---
+++ index.dbe12bab.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#search-form");console.log(e);e.addEventListener("submit",onSearchBtn)}();
+//# sourceMappingURL=index.dbe12bab.js.map
