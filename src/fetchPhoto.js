294808557148057060a65e37d403a54b5e6e4085
OlenaDidkivska <elenatyutyuma@gmail.com>
2022-09-18 00:17:34 +0300

    add scroll

--- src/fetchPhoto.js
+++ src/fetchPhoto.js
@@ -1,24 +1,21 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com';
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '29843828-e519b5089d649383ab6008921';
 export async function fetchPhoto(title, page) {
   try {
-    const response = await axios.get(
-      `${BASE_URL}/api/?key=29843828-e519b5089d649383ab6008921`,
-      {
-        params: {
-          q: `${title}`,
-          image_type: 'photo',
-          orientation: 'horizontal',
-          safesearch: true,
-          per_page: 40,
-          page: `${page}`,
-        },
-      }
-    );
-    const photoItems = response;
-    console.log(`GET: Here's the list of todos`, photoItems);
-    return photoItems;
+    const response = await axios.get(`${BASE_URL}?`, {
+      params: {
+        key: `${KEY}`,
+        q: `${title}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: `${page}`,
+      },
+    });
+    return response.data;
   } catch (errors) {
     console.error(errors);
   }
--- src/index.js
+++ src/index.js
@@ -1,44 +1,50 @@
-import throttle from 'lodash.throttle';
-
+const axios = require('axios');
+import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
-const axios = require('axios');
 import { fetchPhoto } from './fetchPhoto';
-import Notiflix from 'notiflix';
+import throttle from 'lodash.throttle';
 
-const form = document.querySelector('#search-form');
-const gallery = document.querySelector('div.gallery');
-const info = document.querySelectorAll('.gallery .info');
-const request = document.querySelector('input');
+const refs = {
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('div.gallery'),
+  request: document.querySelector('input'),
+};
+
+const { form, gallery, request } = refs;
 
 let page = 1;
 
 async function onSearchBtn(e) {
   e.preventDefault();
 
-  if (request.value) {
-    fetchPhoto(request.value, page)
-      .then(renderPhotoCard)
-      .catch(error => {
-        console.log(error);
-      });
-  } else {
+  if (!request.value) {
     gallery.textContent = '';
     onFetchError();
+    return;
   }
+
+  fetchPhoto(request.value, page)
+    .then(renderPhotoCard)
+    .then(notificationTotalHits)
+    .catch(error => {
+      console.log(error);
+    });
 }
 
-async function renderPhotoCard(response) {
-  const images = response.data.hits;
-  console.log(response);
-  console.log(images);
+function notificationTotalHits(photo) {
+  Notiflix.Notify.success(`Hooray! We found ${photo.totalHits} images.`);
+}
 
-  if (images.length === 0) {
+async function renderPhotoCard(photo) {
+  if (photo.hits.length === 0) {
     onFetchError();
+    return;
   }
 
-  const marcup = images.reduce(
+  page += 1;
+
+  const marcup = photo.hits.reduce(
     (
       acc,
       { webformatURL, largeImageURL, tags, likes, views, comments, downloads }
@@ -66,23 +72,36 @@ async function renderPhotoCard(response) {
 
   gallery.insertAdjacentHTML('beforeend', marcup);
 
-  if (marcup !== '') {
-    // console.log(page);
-    Notiflix.Notify.info(`Hooray! We found ${photo.totalHits} images.`);
-    gallery.innerHTML = await marcup;
-    let lightbox = new SimpleLightbox('.gallery a', {
-      captionDelay: 250,
-      captionsData: 'alt',
-    });
-    return page;
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+  });
+
+  return photo;
+}
+
+function onScroll() {
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight
+  ) {
+    fetchPhoto(request.value, page)
+      .then(checkPhotoList)
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
 
-function onFetchError() {
-  Notiflix.Notify.failure(
-    'Sorry, there are no images matching your search query. Please try again.'
-  );
-  return;
+function checkPhotoList(images) {
+  if (images.hits.length === 0) {
+    Notify.warning('You have reached the end of the list');
+    window.removeEventListener('scroll', onScroll);
+    return;
+  } else {
+    renderPhotoCard(images);
+    scrollBy();
+  }
 }
 
 function scrollBy() {
@@ -96,49 +115,12 @@ function scrollBy() {
   });
 }
 
-info.forEach(element => (element.style.display = 'flex'));
-
-// function onScroll() {
-//   scrollBy();
-//   if (
-//     window.scrollY + window.innerHeight >=
-//     document.documentElement.scrollHeight
-//   ) {
-//     fetchPhoto(request.value, page).then(response => {
-//       if (response.hits.length === 0) {
-//         Notify.warning('You have reached the end of the list');
-//         window.removeEventListener('scroll', onScroll);
-//         return;
-//       } else {
-//         renderPhotoCard(response);
-//       }
-//     });
-//   }
-// }
-
-// function loadMore() {
-//   return pixabayAPI.fetchImages(searchParams);
-// }
-
-async function checkPosition() {
-  page += 1;
-  if (
-    window.scrollY + window.innerHeight >=
-    document.documentElement.scrollHeight
-  ) {
-    fetchPhoto(request.value, page).then(response => {
-      console.log(response.hits);
-      if (response.data.hits.length === 0) {
-        window.removeEventListener('scroll', checkPosition);
-        Notify.warning('You have reached the end of the list');
-        return;
-      } else {
-        renderPhotoCard(response);
-        scrollBy();
-      }
-    });
-  }
-}
-
 form.addEventListener('submit', onSearchBtn);
-window.addEventListener('scroll', throttle(checkPosition, 300));
+window.addEventListener('scroll', onScroll);
+
+function onFetchError() {
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+  return;
+}
